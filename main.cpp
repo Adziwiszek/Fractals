d3d0ef99def2f3ebf2972a20495d04f91e0c8a29
Adam Dziwiszek <adziwiszek1@gmail.com>
2024-11-02 12:45:42 +0100

    first working version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
+#include <unistd.h>
 
 #define DEB(text) std::cout << "D! " << text << std::endl;
 
 const int WIDTH{800};
 const int HEIGHT{600};
-const int PIXEL_SIZE{20};
+const int PIXEL_SIZE{1};
 
 int gridWidth{WIDTH / PIXEL_SIZE};
 int gridHeight{HEIGHT / PIXEL_SIZE};
@@ -13,7 +16,6 @@ int gridHeight{HEIGHT / PIXEL_SIZE};
 typedef sf::Vector2f Vec2;
 typedef sf::Vector2i IVec2;
 
-sf::VertexArray makeGrid(const int W, const int H);
 void setPixelColor(const int x, const int y, sf::Color color); 
 sf::Color getGradientColor(const int x, const int max);
 
@@ -24,49 +26,28 @@ float mod2(Vec2 z) {
 
 class Fractal {
 public:
-    Fractal();
+    sf::VertexArray grid;
+    Fractal(const int W, const int H);
     void render(IVec2 renderSize, Vec2 constant);
+
 private:
-    sf::VertexArray grid = makeGrid(gridWidth, gridHeight);
-    
+    sf::Vector2u size;
+
     Vec2 computeNext(Vec2 current, Vec2 constant);
     int computeIterations(Vec2 z0, Vec2 constant, int maxIterations);
+    void setPixelColor(const int x, const int y, sf::Color color);
 };
 
-sf::VertexArray grid = makeGrid(gridWidth, gridHeight);
-
-
-void render(IVec2 renderSize, Vec2 constant) {
-    const float scale{1.0f / float(static_cast<float>(renderSize.y) / 2.0)};
-    const int maxIter{50};
-    DEB("start rendering..."); 
-    for(int y{0}; y < renderSize.y; y++) {
-        for(int x{0}; x < renderSize.x; x++) {
-            // compute pixel coordinates
-            float px {float(x - renderSize.x) * scale};
-            float py {float(y - renderSize.y) * scale};
-            int gridX {x / PIXEL_SIZE};
-            int gridY {y / PIXEL_SIZE};
-            // compute color
-            if(gridX < 0 || gridX >= gridHeight || gridY < 0 || gridY >= gridHeight) {
-                DEB(gridX);
-                continue;
-            }
-            const float iterations = computeIterations({px, py}, constant, maxIter);
-            DEB(iterations);
-            setPixelColor(px, py, getGradientColor(iterations, maxIter)); 
-        }
-    }
-    DEB("finished!");
-}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Fractals");
     window.setFramerateLimit(60);
-
-
-    //sf::VertexArray grid = makeGrid(gridWidth, gridHeight);
-    render({800, 600}, {1.0, 2.0});
+    DEB(gridHeight);
+    DEB(gridWidth);
+    auto fractal = Fractal(gridWidth, gridHeight);
+    fractal.render({gridWidth, gridHeight}, {-0.78, 0.1});
+    Vec2 c{1.0, -1.0};
+    bool up{true};
 
     while (window.isOpen()) {
         sf::Event event;
@@ -77,18 +58,33 @@ int main() {
                     event.key.code == sf::Keyboard::Q)
                 window.close();
         }
-
+        if(!up)
+            c += Vec2(-0.01, 0.01);  
+        else 
+            c += Vec2(0.01, -0.01);  
+        fractal.render({gridWidth, gridHeight}, c);
+        if(c.x >= 0.5) up = false;
+        if(c.x <= -0.5) up = true;
+        DEB(c.x);
+        //std::this_thread::sleep_for(std::chrono::milliseconds(200));
         window.clear(sf::Color::Black);
-        
-        window.draw(grid);
-
+        window.draw(fractal.grid); 
         window.display();
     }
     return 0;
 }
 
-Fractal::Fractal() {}
-
+Fractal::Fractal(const int W, const int H)
+    : grid{sf::PrimitiveType::Points, size_t(W * H)},
+      size{unsigned(W), unsigned(H)} {
+    uint32_t idx{0};
+    for(uint32_t x{0}; x < W; x++) {
+        for(uint32_t y{0}; y < H; y++) {
+            grid[idx++].position = 
+                sf::Vector2f{static_cast<float>(x), static_cast<float>(y)};
+        }
+    }
+}
 
 Vec2 Fractal::computeNext(Vec2 current, Vec2 constant) {
     float zr = current.x * current.x  - current.y * current.y;
@@ -106,34 +102,24 @@ int Fractal::computeIterations(Vec2 z0, Vec2 constant, int maxIterations) {
     return iterations;
 }
 
-sf::VertexArray makeGrid(const int W, const int H) {
-    sf::VertexArray grid(sf::Quads, W * H * 4);   
-    for (int x = 0; x < W; ++x) {
-        for (int y = 0; y < H; ++y) {
-            int index{(x + y * W) * 4};
-        
-            // Define the quad for the current pixel
-            grid[index + 0].position = sf::Vector2f(x * PIXEL_SIZE, y * PIXEL_SIZE);
-            grid[index + 1].position = sf::Vector2f((x + 1) * PIXEL_SIZE, y * PIXEL_SIZE);
-            grid[index + 2].position = sf::Vector2f((x + 1) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE);
-            grid[index + 3].position = sf::Vector2f(x * PIXEL_SIZE, (y + 1) * PIXEL_SIZE);
 
-            // Set an initial color for each pixel
-            grid[index + 0].color = sf::Color::White;
-            grid[index + 1].color = sf::Color::White;
-            grid[index + 2].color = sf::Color::White;
-            grid[index + 3].color = sf::Color::White;
+void Fractal::render(IVec2 renderSize, Vec2 constant) {
+    const float scale{1.0f / float(static_cast<float>(renderSize.y) / 2.0)};
+    const int maxIter{50};
+    for(int y{0}; y < renderSize.y; y++) {
+        for(int x{0}; x < renderSize.x; x++) {
+            // compute pixel coordinates
+            float px {float(x - renderSize.x / 2.0) * scale};
+            float py {float(y - renderSize.y / 2.0) * scale};
+            const float iterations = computeIterations({px, py}, constant, maxIter);
+            setPixelColor(x, y, getGradientColor(iterations, maxIter)); 
         }
     }
-    return grid;
 }
 
-void setPixelColor(const int x, const int y, sf::Color color) {
-    int index {(x + y * gridWidth) * 4};
-    grid[index + 0].color = color;
-    grid[index + 1].color = color;
-    grid[index + 2].color = color;
-    grid[index + 3].color = color;
+void Fractal::setPixelColor(const int x, const int y, sf::Color color) {
+    int idx {x + y * int(size.x)};
+    grid[idx].color = color;
 }
 
 sf::Color getGradientColor(const int x, const int max) {
